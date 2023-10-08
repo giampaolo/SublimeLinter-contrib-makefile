37be7b7d08914d97e8434b2682cab4072713125e
Giampaolo Rodola <g.rodola@gmail.com>
2023-10-08 22:41:33 +0200

    recognize bad fun calls

--- linter.py
+++ linter.py
@@ -1,3 +1,5 @@
+import re
+
 import sublime
 
 from SublimeLinter.lint import Linter
@@ -10,6 +12,17 @@ SPECIAL_VARS = {
     "MAKEFILE_LIST",
 }
 
+REGEX_FUN_CALL = re.compile(
+    r"""
+    ^\t\$[{|\(]\s*  # open parenthesis
+    MAKE
+    \s*[}|\)]  # close parenthesis
+    \s+
+    ([A-Za-z0-9_\-]+)
+    """,
+    re.VERBOSE,
+)
+
 
 def global_vars(view):
     # the `VARIABLE`s declared in the global namespace
@@ -17,7 +30,7 @@ def global_vars(view):
     return set([view.substr(x) for x in regions])
 
 
-def functions(view):
+def function_names(view):
     # the functions / targets
     regions = view.find_by_selector("entity.name.function")
     return set([view.substr(x) for x in regions])
@@ -34,6 +47,12 @@ def region_position(view, region):
     return (x, y, z)
 
 
+def readlines(view):
+    region = sublime.Region(0, view.size())
+    return [view.substr(x) for x in view.lines(region)]
+
+
+
 class Parser:
     def __init__(self):
         self.view = sublime.active_window().active_view()
@@ -41,6 +60,7 @@ class Parser:
 
     def run(self):
         self.find_undefined_names()
+        self.find_undefined_fun_calls()
         return self.matches
 
     def add(self, pos, msg, type="error"):
@@ -56,7 +76,10 @@ class Parser:
         self.matches.append(lm)
 
     def find_undefined_names(self):
-        # find all undefined names (e.g. undefined `$(FOO)`)
+        # Find all undefined names, e.g. FOO does not exist:
+        #
+        # some-target:
+        #     echo $(FOO)
         view = self.view
         gvars = global_vars(view)
         for region in referenced_vars(view):
@@ -64,6 +87,22 @@ class Parser:
             if name not in gvars and name not in SPECIAL_VARS:
                 pos = region_position(view, region)
                 self.add(pos, "undefined name `%s`" % name)
+
+    def find_undefined_fun_calls(self):
+        # Find all undefined names, e.g. unknown-target does not exist):
+        #
+        # some-target:
+        #     ${MAKE} unknown-target
+        fnames = function_names(self.view)
+        for idx, line in enumerate(readlines(self.view)):
+            m = re.match(REGEX_FUN_CALL, line)
+            if m:
+                fun_name = m.group(1)
+                if fun_name not in fnames:
+                    pos = idx, 1, len(line)
+                    self.add(pos, "undefined target `%s`" % fun_name)
+
+
 
 
 class Makefile(Linter):
