953fcbe6b69c351458b462b98a7fa2670897a894
Giampaolo Rodola <g.rodola@gmail.com>
2023-10-08 20:54:04 +0200

    first draft recognizing undefined NAMEs
    Signed-off-by: Giampaolo Rodola <g.rodola@gmail.com>

--- linter.py
+++ linter.py
@@ -1,10 +1,62 @@
-from SublimeLinter.lint import Linter  # or NodeLinter, PythonLinter, ComposerLinter, RubyLinter
+import sublime
+
+from SublimeLinter.lint import Linter
+from SublimeLinter.lint.linter import LintMatch
 
 
-class __class__(Linter):
-    cmd = '__cmd__'
-    regex = r''
-    multiline = False
-    defaults = {
-        'selector': 'source.python'
-    }
+# https://www.gnu.org/software/make/manual/html_node/Special-Variables.html
+SPECIAL_VARS = {
+    "MAKE",
+    "MAKEFILE_LIST",
+}
+
+
+def global_vars(view):
+    # the `VARIABLE`s declared in the global namespace
+    regions = view.find_by_selector("variable.other.makefile")
+    return set([view.substr(x) for x in regions])
+
+
+def functions(view):
+    # the functions / targets
+    regions = view.find_by_selector("entity.name.function")
+    return set([view.substr(x) for x in regions])
+
+
+def referenced_vars(view):
+    regions = view.find_by_selector("variable.parameter.makefile")
+    return regions
+
+
+def region_position(view, region):
+    x, y = view.rowcol(region.begin())
+    z = y + (region.end() - region.begin())
+    return (x, y, z)
+
+
+class Makefile(Linter):
+    cmd = None
+    regex = "stub"
+    defaults = {"selector": "source.makefile"}
+
+    def run(self, _cmd, _code):
+        return "stub"  # just to trigger find_errors()
+
+    def find_errors(self, _output):
+        def add(pos, msg):
+            lineno, col, end_col = pos
+            lm = LintMatch(line=lineno, col=col, end_col=end_col, message=msg)
+            matches.append(lm)
+
+        view = sublime.active_window().active_view()
+        matches = []
+
+        # find all unreferenced vars
+        gvars = global_vars(view)
+        for region in referenced_vars(view):
+            name = view.substr(region)
+            if name not in gvars and name not in SPECIAL_VARS:
+                pos = region_position(view, region)
+                add(pos, "undefined name `%s`" % name)
+
+        return matches
